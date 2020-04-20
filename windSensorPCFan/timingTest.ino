08534e7340da6b16e766a939fbbe343368849eee
k/bit <biters.k@gmail.com>
2020-04-20 17:36:37 +0300

    update

--- windSensorPCFan/timingTest.ino
+++ windSensorPCFan/timingTest.ino
@@ -1,11 +1,31 @@
+
+#define analogPinForRV 1 // change to pins you the analog pins are using
+#define analogPinForTMP 0
+const float zeroWindAdjustment = .2; // negative numbers yield smaller wind speeds and vice versa.
+int TMP_Therm_ADunits;               //temp termistor value from wind sensor
+float RV_Wind_ADunits;               //RV output from wind sensor
+float RV_Wind_Volts;
+unsigned long lastMillis;
+int TempCtimes100;
+float zeroWind_ADunits;
+float zeroWind_volts;
+float WindSpeed_MPH;
+
+float maxWindSpeed = 50.00; // <---------------------- ieraksti maximālo vēju ātrumu ar komatu piem: 63.55, 50.0;
+int speed_ = 0;
+int highestValue = 0;
+bool newHighValue = true;
+int slowDownValue = 1;
+int lowestSpeedValue = 1;
+
+// ----------- //
+
 unsigned long lastMillis_readSensor;
 unsigned long lastMillis_checkHiVal;
 unsigned long lastMillis_buildNewSpeed;
-unsigned long lastMillis_simulate;
-unsigned long lastMillis_decreaseFanSpeed;
+unsigned long lastMillis_spinningFans;
 
-int sensorValue = 10000;
-int highestValue = 0;
+int sensorValue = 10000; 
 int currentValue = 10000;
 int dreceasingFanSpeedMillis = 100; // millisecond interval in which currentValue will be decreased by 1
 int minimalValue = 1;               // treshol value so smal curent changes dosn't start SetUpNewSpeed(); function
@@ -13,12 +33,18 @@ float timeToBuildUpNewSpeed = 2.5;  // change this value to increase the time, s
 bool newhigGestValue = false;
 bool firstFrameSetupSpeed = false;
 bool fansAreBuildingUp = false;
-bool fansAreDecreasingSpeed = false;
+bool fansAreSpinning = false;
 
-void setup(){
-      
+void setup()
+{
     Serial.begin(57600);
- Serial.println("start");
+    Serial.println("start");
+
+    // ----------- //
+
+    pinMode(A2, INPUT);    // GND pin
+    pinMode(A3, INPUT);    // VCC pin
+    digitalWrite(A3, LOW); // turn off pullups
 }
 
 void loop()
@@ -29,36 +55,33 @@ void loop()
     {
         SetupNewSpeed();
     }
-
-    if (!newhigGestValue && !fansAreBuildingUp)
+    if (fansAreSpinning)
     {
-        DecreasingFanSpeed();
+        SpinningFans();
     }
 }
 
-
-
-void DecreasingFanSpeed()
+void SpinningFans()
 {
-    if (millis() - lastMillis_decreaseFanSpeed > dreceasingFanSpeedMillis)
+    if (millis() - lastMillis_spinningFans > 3000)
     {
-        currentValue--;
-        lastMillis_decreaseFanSpeed = millis();
+        fansAreSpinning = false;
+        lastMillis_spinningFans = millis();
+        currentValue =0;
+        analogWrite(9, currentValue);
+
     }
-    if(currentValue <= minimalValue){
-        fansAreDecreasingSpeed = false;
-    }
-    //Serial.println(currentValue);
 }
 
 void SetupNewSpeed()
-{   
+{
     // functions Setup
     if (firstFrameSetupSpeed)
     {
         lastMillis_buildNewSpeed = millis();
         firstFrameSetupSpeed = false;
         fansAreBuildingUp = true;
+        analogWrite(9, currentValue);
     }
 
     // time to finish building up the new speed
@@ -66,7 +89,7 @@ void SetupNewSpeed()
     {
         fansAreBuildingUp = false;
         highestValue = false;
-        fansAreDecreasingSpeed = true;
+        fansAreSpinning = true;
     }
 }
 
@@ -74,7 +97,7 @@ void CheckHighestValue()
 {
     if (sensorValue > highestValue)
     {
-        Serial.println("new highestValue: "+ sensorValue);
+        Serial.println("new highestValue: " + sensorValue);
         highestValue = sensorValue;
     }
 
@@ -85,8 +108,7 @@ void CheckHighestValue()
         {
             newhigGestValue = true;
             firstFrameSetupSpeed = true;
-            currentValue = newhigGestValue; // convertation needed !!
-            fansAreDecreasingSpeed = false;
+            currentValue = newhigGestValue;
         }
         lastMillis_checkHiVal = millis();
     }
@@ -96,17 +118,33 @@ void ReadSensor()
 {
     if (millis() - lastMillis_readSensor > 200)
     {
-        SimulateSensorValue();
+        lastMillis_readSensor = millis();
+
+        TMP_Therm_ADunits = analogRead(analogPinForTMP);
+        RV_Wind_ADunits = analogRead(analogPinForRV);
+        RV_Wind_Volts = (RV_Wind_ADunits * 0.0048828125);
+        TempCtimes100 = (0.005 * ((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits)) - (16.862 * (float)TMP_Therm_ADunits) + 9075.4;
+        zeroWind_ADunits = -0.0006 * ((float)TMP_Therm_ADunits * (float)TMP_Therm_ADunits) + 1.0727 * (float)TMP_Therm_ADunits + 47.172; //  13.0C  553  482.39
+        zeroWind_volts = (zeroWind_ADunits * 0.0048828125) - zeroWindAdjustment;
+        WindSpeed_MPH = pow(((RV_Wind_Volts - zeroWind_volts) / .2300), 2.7265);
+
+        sensorValue = map(WindSpeed_MPH, 0, maxWindSpeed, 0, 255);
+        PrintStuff();
+
         lastMillis_readSensor = millis();
     }
 }
 
-void SimulateSensorValue()
+void PrintStuff()
 {
-    if (millis() - lastMillis_simulate > 200)
-    {
-        lastMillis_simulate = millis();
-        sensorValue = random(0, 1024);
-        // Serial.println("new sensor valu: "+ sensorValue)
-    }
+    Serial.print("  TMP volts ");
+    Serial.print(TMP_Therm_ADunits * 0.0048828125);
+    Serial.print(" RV volts ");
+    Serial.print((float)RV_Wind_Volts);
+    Serial.print("\t  TempC*100 ");
+    Serial.print(TempCtimes100);
+    Serial.print("   ZeroWind volts ");
+    Serial.print(zeroWind_volts);
+    Serial.print("   WindSpeed MPH ");
+    Serial.println((float)WindSpeed_MPH);
 }
